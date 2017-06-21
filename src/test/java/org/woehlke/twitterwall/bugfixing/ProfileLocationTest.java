28d34e094ceb8a6cdaf8225b2f6bdb2faa59f838
phasenraum2010 <thomas@woehlke.org>
2017-06-21 15:46:51 +0200

    working on: #34

--- src/test/java/org/woehlke/twitterwall/bugfixing/ProfileLocationTest.java
+++ src/test/java/org/woehlke/twitterwall/bugfixing/ProfileLocationTest.java
@@ -4,5 +4,5 @@ package org.woehlke.twitterwall.bugfixing;
  * Created by tw on 20.06.17.
  */
 public class ProfileLocationTest {
-    //TODO: #44
+    //TODO: #44 https://github.com/phasenraum2010/twitterwall2/issues/44
 }
--- src/test/java/org/woehlke/twitterwall/bugfixing/ProfileUrlTest.java
+++ src/test/java/org/woehlke/twitterwall/bugfixing/ProfileUrlTest.java
@@ -4,5 +4,5 @@ package org.woehlke.twitterwall.bugfixing;
  * Created by tw on 20.06.17.
  */
 public class ProfileUrlTest {
-    //TODO: #43
+    //TODO: #43 https://github.com/phasenraum2010/twitterwall2/issues/43
 }
