7741de35469a4c1ab7c17737f445768ebe9d381e
phasenraum2010 <thomas@woehlke.org>
2017-06-29 05:39:34 +0200

    refactoring, code cleanup, preparing spring-batch

--- src/test/java/org/woehlke/twitterwall/bugfixing/ProfileLocationTest.java
+++
@@ -1,8 +0,0 @@
-package org.woehlke.twitterwall.bugfixing;
-
-/**
- * Created by tw on 20.06.17.
- */
-public class ProfileLocationTest {
-    //TODO: #44 https://github.com/phasenraum2010/twitterwall2/issues/44
-}
--- src/test/java/org/woehlke/twitterwall/bugfixing/ProfileUrlTest.java
+++
@@ -1,8 +0,0 @@
-package org.woehlke.twitterwall.bugfixing;
-
-/**
- * Created by tw on 20.06.17.
- */
-public class ProfileUrlTest {
-    //TODO: #43 https://github.com/phasenraum2010/twitterwall2/issues/43
-}
