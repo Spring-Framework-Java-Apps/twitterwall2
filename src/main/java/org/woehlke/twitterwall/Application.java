c4ec9b414545c078eed946b4d900d5c1b1b5ff05
phasenraum2010 <thomas@woehlke.org>
2017-07-18 01:21:51 +0200

    working on #122

--- src/main/java/org/woehlke/twitterwall/Application.java
+++ src/main/java/org/woehlke/twitterwall/Application.java
@@ -2,21 +2,15 @@ package org.woehlke.twitterwall;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 /**
  * Created by tw on 10.06.17.
  */
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
-@IntegrationComponentScan
 @EnableScheduling
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/org/woehlke/twitterwall/ConfigTwitterwall.java
@@ -0,0 +1,32 @@
+package org.woehlke.twitterwall;
+
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Created by tw on 17.07.17.
+ */
+//@ConfigurationProperties(prefix="twitterwall")
+public class ConfigTwitterwall {
+
+    /*
+
+    private Frontend frontend;
+
+    private static class Frontend {
+
+        private String theme;
+
+        private String maxResults;
+
+        private Info info;
+
+        private static class Info {
+
+            private String webpage;
+
+        }
+
+    }
+
+    */
+}
