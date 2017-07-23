5d7690b32d4bde53e9bb2ef2573163723c78cc6d
phasenraum2010 <thomas@woehlke.org>
2017-07-23 12:53:30 +0200

    working on #138

--- src/main/java/org/woehlke/twitterwall/conf/ActiveMQConfig.java
+++ src/main/java/org/woehlke/twitterwall/conf/ActiveMQConfig.java
@@ -9,27 +9,60 @@ import javax.jms.Queue;
 @Configuration
 public class ActiveMQConfig {
 
-    public static final String HELLO_QUEUE = "hello.queue";
+    @Bean
+    public Queue startTaskChannelJMSQueue() {
+        return new ActiveMQQueue(QUEUE_START_TASK);
+    }
 
     @Bean
-    public Queue helloJMSQueue() {
+    public Queue startUpdateTweetsChannelJMSQueue() {
+        return new ActiveMQQueue(QUEUE_UPDATE_TWEETS);
+    }
 
-        return new ActiveMQQueue(HELLO_QUEUE);
+    @Bean
+    public Queue startUpdateUserProfilesChannelJMSQueue() {
+        return new ActiveMQQueue(QUEUE_UPDATE_USERS);
+    }
 
+    @Bean
+    public Queue startUpdateUserProfilesFromMentionsChannelJMSQueue() {
+        return new ActiveMQQueue(QUEUE_UPDATE_USERS_FROM_NENTIONS);
+    }
+
+    @Bean
+    public Queue startFetchTweetsFromTwitterSearchChannelJMSQueue() {
+        return new ActiveMQQueue(QUEUE_FETCH_TWEETS_FROM_TWITTER_SEARCH);
+    }
+
+    @Bean
+    public Queue startFetchUsersFromDefinedUserListChannelJMSQueue() {
+        return new ActiveMQQueue(HELLO_QUEUE_FETCH_USERS_FROM_LIST);
+    }
+
+    @Bean
+    public Queue startFetchTestDataForUserChannelJMSQueue() {
+        return new ActiveMQQueue(HELLO_QUEUE_FETCH_TESTDATA_USERS);
+    }
+
+    @Bean
+    public Queue startFetchTestDataForTweetsChannelJMSQueue() {
+        return new ActiveMQQueue(HELLO_QUEUE_FETCH_TESDATA_TWEETS);
+    }
+
+    @Bean
+    public Queue startFetchImprintUserChannelJMSQueue() {
+        return new ActiveMQQueue(HELLO_QUEUE_FETCH_IMPRINT_USER);
     }
 
 
-    public static final String QUEUE_START_TASK = "startTaskChannel";
-
-    public static final String QUEUE_UPDATE_TWEETS = "startUpdateTweetsChannel";
-    public static final String QUEUE_UPDATE_USERS =  "startUpdateUserProfilesChannel";
-    public static final String QUEUE_UPDATE_USERS_FROM_NENTIONS = "startUpdateUserProfilesFromMentionsChannel";
-
-    public static final String QUEUE_FETCH_TWEETS_FROM_TWITTER_SEARCH =    "startFetchTweetsFromTwitterSearchChannel";
-    public static final String HELLO_QUEUE_FETCH_USERS_FROM_LIST =    "startFetchUsersFromDefinedUserListChannel";
-    public static final String HELLO_QUEUE_FETCH_TESTDATA_USERS =    "startFetchTestDataForUserChannel";
-    public static final String HELLO_QUEUE_FETCH_TESDATA_TWEETS =    "startFetchTestDataForTweetsChannel" ;
-    public static final String HELLO_QUEUE_FETCH_IMPRINT_USER =    "startFetchImprintUserChannel"    ;
-
+    public static final String QUEUE_START_TASK = "startTaskChannel.queue";
+    public static final String QUEUE_UPDATE_TWEETS = "startUpdateTweetsChannel.queue";
+    public static final String QUEUE_UPDATE_USERS =  "startUpdateUserProfilesChannel.queue";
+    public static final String QUEUE_UPDATE_USERS_FROM_NENTIONS = "startUpdateUserProfilesFromMentionsChannel.queue";
+    public static final String QUEUE_FETCH_TWEETS_FROM_TWITTER_SEARCH =    "startFetchTweetsFromTwitterSearchChannel.queue";
+    public static final String HELLO_QUEUE_FETCH_USERS_FROM_LIST =    "startFetchUsersFromDefinedUserListChannel.queue";
+    public static final String HELLO_QUEUE_FETCH_TESTDATA_USERS =    "startFetchTestDataForUserChannel.queue";
+    public static final String HELLO_QUEUE_FETCH_TESDATA_TWEETS =    "startFetchTestDataForTweetsChannel.queue";
+    public static final String HELLO_QUEUE_FETCH_IMPRINT_USER =    "startFetchImprintUserChannel.queue";
 
 }
