b521f1b3d5ea1f89490c78b25ab9fc84c6eaed3e
phasenraum2010 <thomas@woehlke.org>
2017-07-29 21:00:28 +0200

    working on #157

--- src/main/java/org/woehlke/twitterwall/oodm/repositories/custom/impl/MediaRepositoryImpl.java
+++ src/main/java/org/woehlke/twitterwall/oodm/repositories/custom/impl/MediaRepositoryImpl.java
@@ -1,6 +1,7 @@
 package org.woehlke.twitterwall.oodm.repositories.custom.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.woehlke.twitterwall.oodm.entities.Media;
 import org.woehlke.twitterwall.oodm.repositories.custom.MediaRepositoryCustom;
 
@@ -8,6 +9,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
+@Repository
 public class MediaRepositoryImpl implements MediaRepositoryCustom {
 
     private final EntityManager entityManager;
--- src/main/java/org/woehlke/twitterwall/oodm/repositories/custom/impl/TaskRepositoryImpl.java
+++ src/main/java/org/woehlke/twitterwall/oodm/repositories/custom/impl/TaskRepositoryImpl.java
@@ -1,6 +1,7 @@
 package org.woehlke.twitterwall.oodm.repositories.custom.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.woehlke.twitterwall.oodm.entities.Task;
 import org.woehlke.twitterwall.oodm.repositories.custom.TaskRepositoryCustom;
 
@@ -8,6 +9,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
+@Repository
 public class TaskRepositoryImpl implements TaskRepositoryCustom {
 
     private final EntityManager entityManager;
