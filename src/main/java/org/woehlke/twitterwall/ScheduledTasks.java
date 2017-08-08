b8f511f9324ce47bdeb10c20a5f2568e6f31e0ae
phasenraum2010 <thomas@woehlke.org>
2017-08-08 22:22:27 +0200

    fixed #240

--- src/main/java/org/woehlke/twitterwall/ScheduledTasks.java
+++ src/main/java/org/woehlke/twitterwall/ScheduledTasks.java
@@ -18,7 +18,7 @@ public class ScheduledTasks {
     @Scheduled(initialDelay= TEN_SECONDS, fixedRate = FIXED_RATE_FOR_SCHEDULAR_FETCH_TWEETS)
     public void fetchTweetsFromTwitterSearch() {
         String msg = "fetch Tweets From TwitterSearch ";
-        if(schedulerProperties.getAllowUpdateTweets()  && !schedulerProperties.getSkipFortesting()) {
+        if((schedulerProperties.getAllowUpdateTweets())  && (!schedulerProperties.getSkipFortesting())) {
             Task task = asyncStartTask.fetchTweetsFromSearch();
             log.info(msg+ "SCHEDULED: task "+task.getUniqueId());
         }
@@ -27,7 +27,7 @@ public class ScheduledTasks {
     @Scheduled(initialDelay= TEN_SECONDS *2, fixedRate = FIXED_RATE_FOR_SCHEDULAR_FETCH_USER_LIST)
     public void fetchUsersFromDefinedUserList(){
         String msg = "fetch Users from Defined User List ";
-        if(schedulerProperties.getFetchUserListAllow()  && !schedulerProperties.getSkipFortesting()) {
+        if((schedulerProperties.getFetchUserListAllow()) && (!schedulerProperties.getSkipFortesting())) {
             Task task = asyncStartTask.fetchUsersFromList();
             log.info(msg+ "SCHEDULED: task "+task.getUniqueId());
         }
@@ -36,7 +36,7 @@ public class ScheduledTasks {
     @Scheduled(initialDelay= TEN_SECONDS *3, fixedRate = FIXED_RATE_FOR_SCHEDULAR_FETCH_FOLLOWER)
     public void fetchFollower(){
         String msg = "fetch Follower ";
-        if(schedulerProperties.getFetchFollowerAllow()  && ! schedulerProperties.getSkipFortesting()) {
+        if((schedulerProperties.getFetchFollowerAllow())  && (!schedulerProperties.getSkipFortesting())) {
             Task task = asyncStartTask.fetchFollower();
             log.info(msg+ "SCHEDULED: task "+task.getUniqueId());
         }
@@ -45,7 +45,7 @@ public class ScheduledTasks {
     @Scheduled(initialDelay= TEN_SECONDS *4, fixedRate = FIXED_RATE_FOR_SCHEDULAR_FETCH_FRIENDS)
     public void fetchFriends(){
         String msg = "fetch Friends ";
-        if(schedulerProperties.getFetchFriendsAllow() && !schedulerProperties.getSkipFortesting()) {
+        if((schedulerProperties.getFetchFriendsAllow()) && (!schedulerProperties.getSkipFortesting())) {
             Task task = asyncStartTask.fetchFriends();
             log.info(msg+ "SCHEDULED: task "+task.getUniqueId());
         }
@@ -54,7 +54,7 @@ public class ScheduledTasks {
     @Scheduled(initialDelay= TEN_SECONDS *5, fixedRate = FIXED_RATE_FOR_SCHEDULAR_REMOVE_OLD_DATA_FROM_STORAGE)
     public void removeOldDataFromStorage(){
         String msg = "remove Old Data From Storage: ";
-        if(schedulerProperties.getRemoveOldDataFromStorageAllow()  && !schedulerProperties.getSkipFortesting()) {
+        if((schedulerProperties.getRemoveOldDataFromStorageAllow())  && (!schedulerProperties.getSkipFortesting())) {
             Task task = asyncStartTask.removeOldDataFromStorage();
             log.info(msg+ "SCHEDULED: task "+task.getUniqueId());
         }
@@ -63,7 +63,7 @@ public class ScheduledTasks {
     @Scheduled(initialDelay= TEN_SECONDS *6, fixedRate = FIXED_RATE_FOR_SCHEDULAR_UPDATE_USER_BY_MENTION)
     public void updateUserProfilesFromMentions(){
         String msg = "update User Profiles From Mentions";
-        if(schedulerProperties.getAllowUpdateUserProfilesFromMention() && !schedulerProperties.getSkipFortesting()) {
+        if((schedulerProperties.getAllowUpdateUserProfilesFromMention()) && (!schedulerProperties.getSkipFortesting())) {
             Task task = asyncStartTask.updateUsersFromMentions();
             log.info(msg+ "SCHEDULED: task "+task.getUniqueId());
         }
@@ -72,7 +72,7 @@ public class ScheduledTasks {
     @Scheduled(initialDelay= TEN_SECONDS *7, fixedRate = FIXED_RATE_FOR_SCHEDULAR_UPDATE_TWEETS)
     public void updateTweets() {
         String msg = "update Tweets ";
-        if(schedulerProperties.getAllowUpdateTweets() && !schedulerProperties.getSkipFortesting()){
+        if((schedulerProperties.getAllowUpdateTweets()) && (!schedulerProperties.getSkipFortesting())){
             Task task = asyncStartTask.updateTweets();
             log.info(msg+ "SCHEDULED: task "+task.getUniqueId());
         }
@@ -81,7 +81,7 @@ public class ScheduledTasks {
     @Scheduled(initialDelay= TEN_SECONDS *8, fixedRate = FIXED_RATE_FOR_SCHEDULAR_UPDATE_USER)
     public void updateUserProfiles() {
         String msg = "update User Profiles ";
-        if(schedulerProperties.getAllowUpdateUserProfiles()  && !schedulerProperties.getSkipFortesting()) {
+        if((schedulerProperties.getAllowUpdateUserProfiles())  && (!schedulerProperties.getSkipFortesting())) {
             Task task = asyncStartTask.updateUsers();
             log.info(msg+ "SCHEDULED: task "+task.getUniqueId());
         }
